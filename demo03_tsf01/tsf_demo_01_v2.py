f81747b219db91f78520a0d832bf372b9fb5f3af
JosephL <343219660@qq.com>
2019-09-28 17:36:48 +0800

    tf 计算图、张量、会话
    tf 计算图、张量、会话 概念的理解 第一次

--- demo03_tsf01/tsf_demo_01_v2.py
+++ demo03_tsf01/tsf_demo_01_v2.py
@@ -1,3 +1,4 @@
+
 import tensorflow as tf
 import os
 
@@ -17,17 +18,20 @@ def simpleAdd(arr1, arr2):
     b = tf.constant(arr2, name="b")
     c = a + b
     # 执行并输出结果
-    with tf.compat.v1.Session() as sess:
-        res = sess.run(c)
+    res = c.eval()
     ## 打印结果
     print(a)
     print(b)
     print(c)
     return res
 
-print("@@ here we go @@!!")
-print(simpleAdd(4, 3))
-print(simpleAdd(2.1, 3.7))
-print(simpleAdd([5.2], [7.4]))
-print(simpleAdd([2.1, 5.2, 4.6, 1.9], [3.7, 7.4, 6.5, 3.3]))
-print(simpleAdd([[1, 2, 3], [4, 5, 6]], [[2, 5, 5], [6, 9, 13]]))
+# 设置来默认后，可以不用每次都用 session 跑数
+# 但好像必须 用 with, try finally 这种方式
+# 为什么 pylint 会报错
+with tf.compat.v1.Session().as_default():
+    print("@@ here we go @@!!")
+    print(simpleAdd(4, 3))
+    print(simpleAdd(2.1, 3.7))
+    print(simpleAdd([5.2], [7.4]))
+    print(simpleAdd([2.1, 5.2, 4.6, 1.9], [3.7, 7.4, 6.5, 3.3]))
+    print(simpleAdd([[1, 2, 3], [4, 5, 6]], [[2, 5, 5], [6, 9, 13]]))
